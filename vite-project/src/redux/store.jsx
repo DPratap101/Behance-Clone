7c1eb4738f0302336b24797aa9c229dabe490080
Pratap Kumar <dpratap.360@gmail.com>
2024-10-02 19:51:38 +0530

    Delete vite-project directory

--- vite-project/src/redux/store.jsx
+++
@@ -1,7 +0,0 @@
-import { rootReducer } from "./rootReducer";
-import {legacy_createStore} from "redux"
-import {applyMiddleware} from "redux"
-import logger from "redux-logger"
-import {thunk} from "redux-thunk"
-
-export let store=legacy_createStore(rootReducer,applyMiddleware(logger,thunk))
