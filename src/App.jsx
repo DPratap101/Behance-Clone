1f3383bc60d2f6c7e636afeb4d7f65b6b3c36cce
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-10-03 23:06:08 +0530

    Done signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import UserContent from "./components/UserContent";
 import Footer from "./components/footer/Footer";
+import SignIn from './components/SignIn'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <UserContent />
       {/* <Footer /> */}
+      {/* <SignIn /> */}
     </>
   );
 }
