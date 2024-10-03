3fb9697fe51f9afc51a284fc47b72e18db0b45ed
rajatsah95 <rajatsah95@gmail.com>
2024-10-03 10:24:48 +0530

    login and signup page created

---
+++ vite-project/src/pages/login.jsx
@@ -0,0 +1,19 @@
+export let Login=()=>
+{
+    return(
+
+        <div className="rs-container">
+          
+            <form>
+                <h2>Login Page</h2>
+                <input placeholder="email"/>
+                <input  placeholder="password"/>
+                <button type="submit">Submit</button>
+
+            </form>
+              
+
+           
+        </div>
+    )
+}
---
+++ vite-project/src/pages/signup.jsx
@@ -0,0 +1,20 @@
+export let Signup=()=>
+    {
+        return(
+    
+            <div className="rs-container">
+              
+                <form>
+                    <h2>Signup Page</h2>
+                    <input placeholder="username"/>
+                    <input placeholder="email"/>
+                    <input  placeholder="password"/>
+                    <button type="submit">Submit</button>
+    
+                </form>
+                  
+    
+               
+            </div>
+        )
+    }
